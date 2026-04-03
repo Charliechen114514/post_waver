ccb619e6da2c394107604d8176ec44435ec36db8
Charliechen114514 <725610365@qq.com>
2026-04-03 18:43:23 +0800

    fix: converter missing

--- packages/converter-web/vite.config.ts
+++ packages/converter-web/vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: process.env.NODE_ENV === 'production' ? '/post_waver/converter/' : '/',
+  base: process.env.NODE_ENV === 'production' ? '/post_waver/' : '/',
   build: {
     outDir: 'dist',
     emptyOutDir: true
